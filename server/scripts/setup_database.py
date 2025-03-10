cc84599e60c2f011ed1af521888addff2994f11d
Elle0-0 <andreamichellevaz@gmail.com>
2025-03-10 01:03:40 +0000

    lol

--- server/scripts/setup_database.py
+++ server/scripts/setup_database.py
@@ -1,15 +1,16 @@
 import sys
 import os
+import uuid
 
 # Add the project root directory to the system path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, Device, MetricType
+from models import Base, Device, Metric
 
-# Replace with your actual database URL
-DATABASE_URL = f"sqlite:///{os.path.abspath(os.path.join(os.path.dirname(__file__), '../../sysmonitor.db'))}"
+# Load database URL from environment variable
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 def setup_database():
     engine = create_engine(DATABASE_URL)
@@ -19,21 +20,21 @@ def setup_database():
 
     # Add the 'PC' device if it doesn't exist
     if not session.query(Device).filter(Device.name == 'PC').first():
-        device = Device(name='PC', aggregator_id=1, ordinal=1)
+        device = Device(uuid=str(uuid.uuid4()), name='PC')
         session.add(device)
         print("Device 'PC' added to the database.")
 
-    # Add the 'CPU Usage' metric type if it doesn't exist
-    if not session.query(MetricType).filter(MetricType.name == 'CPU Usage').first():
-        metric_type_cpu = MetricType(name='CPU Usage')
+    # Add the 'CPU Usage' metric if it doesn't exist
+    if not session.query(Metric).filter(Metric.name == 'CPU Usage').first():
+        metric_type_cpu = Metric(uuid=str(uuid.uuid4()), name='CPU Usage')
         session.add(metric_type_cpu)
-        print("Metric type 'CPU Usage' added to the database.")
+        print("Metric 'CPU Usage' added to the database.")
 
-    # Add the 'RAM Usage' metric type if it doesn't exist
-    if not session.query(MetricType).filter(MetricType.name == 'RAM Usage').first():
-        metric_type_ram = MetricType(name='RAM Usage')
+    # Add the 'RAM Usage' metric if it doesn't exist
+    if not session.query(Metric).filter(Metric.name == 'RAM Usage').first():
+        metric_type_ram = Metric(uuid=str(uuid.uuid4()), name='RAM Usage')
         session.add(metric_type_ram)
-        print("Metric type 'RAM Usage' added to the database.")
+        print("Metric 'RAM Usage' added to the database.")
 
     session.commit()
     print("Database setup complete.")
