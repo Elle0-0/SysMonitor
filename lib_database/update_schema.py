91b6e26e9ff8daea4da03534694295036db9abff
Elle0-0 <andreamichellevaz@gmail.com>
2025-03-10 18:09:38 +0000

    heh

--- lib_database/update_schema.py
+++ lib_database/update_schema.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Base, ThirdPartyType
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from models import Base, ThirdPartyType
+
 
 # Load database URL from environment variable
 DATABASE_URL = os.getenv('DATABASE_URL')
